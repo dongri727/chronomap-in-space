e102c5ca9052fb142be7210c179123788a063c40
laporte727 <ecole.la.porte@gmail.com>
2024-07-09 18:17:18 +0900

    main

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'my_home_page.dart';
+import 'serverpod_client.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeServerpodClient();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,//左向きを許可
+    DeviceOrientation.landscapeRight,//右向きを許可
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chronomap in Space',
+      theme: ThemeData(
+
+        textTheme: GoogleFonts.tsukimiRoundedTextTheme(
+            Theme.of(context).textTheme
+        ),
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
